b5602381a5fffeb259e5740414dd70f451334486
jpwarren13 <johnpwarren@gmail.com>
2018-10-15 15:52:58 -0500

    Fix eslin on shortly

--- app/config.js
+++ app/config.js
@@ -17,6 +17,8 @@ db.knex.schema.hasTable('urls').then(function(exists) {
       link.string('code', 100);
       link.string('title', 255);
       link.integer('visits');
+      link.integer('user_id').unsigned(); //Added user_id field to associate with urls
+      link.foreign('user_id').references('users.id'); //assigned the user_id to reference id from users table
       link.timestamps();
     }).then(function (table) {
       console.log('Created Table', table);
@@ -36,9 +38,25 @@ db.knex.schema.hasTable('clicks').then(function(exists) {
   }
 });
 
+
+// Added users table that has userID, username, password. Not sure if we need timestamps. 
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 255);
+      user.string('password', 255);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
+
 /************************************************************/
 // Add additional schema definitions below
 /************************************************************/
 
 
 module.exports = db;
+
